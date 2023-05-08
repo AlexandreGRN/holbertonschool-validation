753a06a37a6bd0a9364f75675ed8f07984bfae25
AlexandreGRN <tulkiidra@gmail.com>
2023-05-08 09:58:13 -0700

    apt utils first

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt-get update
 apt-get install apt-utils
+apt-get update
 apt-get install -y hugo make
 apt-get install -y make golang-go git curl
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb -o hugo.deb
