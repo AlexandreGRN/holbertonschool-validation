fdca085cb83eb62cd1dffcd83172a29fb39e158a
AlexandreGRN <tulkiidra@gmail.com>
2023-05-15 06:04:46 -0700

    no mv

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -2,8 +2,6 @@
 apt-get install jq -y
 wget https://github.com/mikefarah/yq/releases/download/v4.5.0/yq_linux_amd64.tar.gz
 tar xzf yq_linux_amd64.tar.gz
-mv yq_linux_amd64 yq
-mv yq /usr/bin/
 yq --version
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
 apt install ./hugo.deb
