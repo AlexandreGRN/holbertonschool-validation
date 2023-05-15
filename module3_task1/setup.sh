170ca53349f29aa0678fb7a4cbfa1d7797345227
AlexandreGRN <tulkiidra@gmail.com>
2023-05-15 06:07:06 -0700

    Sudo everywhere

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-apt-get install jq -y
-wget https://github.com/mikefarah/yq/releases/download/v4.5.0/yq_linux_amd64.tar.gz
-tar xzf yq_linux_amd64.tar.gz
-yq --version
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
-apt install ./hugo.deb
-rm hugo.deb
-rm yq_linux_amd64.tar.gz
+sudo apt-get install jq -y
+sudo wget https://github.com/mikefarah/yq/releases/download/v4.5.0/yq_linux_amd64.tar.gz
+sudo tar xzf yq_linux_amd64.tar.gz
+sudo mv yq_linux_amd64 yq
+sudo mv yq /usr/bin/
+sudo yq --version
+sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
+sudo apt install ./hugo.deb
+sudo rm hugo.deb
+sudo rm yq_linux_amd64.tar.gz
